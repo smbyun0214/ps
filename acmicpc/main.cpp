5fd5b1bf8431461ee1fcf7cfd740a2c2de321656
Seongmok Byeon <smbyun0214@gmail.com>
2019-11-27 18:37:37 +0900

    Initial Commit

---
+++ acmicpc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acmicpc
+//
+//  Created by 변성목 on 2019/11/27.
+//  Copyright © 2019 변성목. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
