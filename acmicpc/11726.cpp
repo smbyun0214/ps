1af6ab1c7c326b3a5127f58efd153fe03b2ee17f
Seongmok Byeon <smbyun0214@gmail.com>
2020-02-05 00:54:41 +0900

    Wed Feb 5

---
+++ acmicpc/11726.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MOD = 10007;
+
+int cache[1001];
+
+int tiling(int width) {
+    if (width <= 1)
+        return 1;
+    
+    int &ret = cache[width];
+    if (ret != -1)
+        return ret;
+    
+    return ret = (tiling(width - 1) + tiling(width - 2)) % MOD;
+}
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    memset(cache, -1, sizeof(cache));
+    
+    int N;
+    cin >> N;
+    
+    cout << tiling(N) << '\n';
+    
+    return 0;
+}
