c7149f4a81cf8d47cda652e7a25341fcb6aae6af
Seongmok Byeon <smbyun0214@gmail.com>
2020-10-25 07:36:25 +0900

    Initialize

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+//
+//  main.cpp
+//  ProblemSolving
+//
+//  Created by 변성목 on 2020/10/25.
+//  Copyright © 2020 변성목. All rights reserved.
+//
+
 #include <iostream>
 
-using namespace std;
-
-int main(int argc, const char * argv[]) {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
-    
-    
+int main (int argc, char *argv[]) {
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
