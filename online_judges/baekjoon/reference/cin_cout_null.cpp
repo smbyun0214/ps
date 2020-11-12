a45cba1f95a1e4fac35b90dccb4fa3d712ea85c8
Seongmok Byeon <smbyun0214@gmail.com>
2020-11-12 11:13:34 +0900

    .

---
+++ online_judges/baekjoon/reference/cin_cout_null.cpp
@@ -0,0 +1,18 @@
+//
+//  cin_cout_null.cpp
+//  ProblemSolving
+//
+//  Created by 변성목 on 2020/11/12.
+//  Copyright © 2020 변성목. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    return 0;
+}
